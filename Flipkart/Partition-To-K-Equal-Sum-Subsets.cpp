c586fb01b3493ba0963985b7dd3676735149fa9b
akgupta0777 <akgupta0777@gmail.com>
2023-01-30 22:52:34 +0530

    [Flipkart]:Partition To K Equal Sum Subsets

--- Flipkart/Partition-To-K-Equal-Sum-Subsets.cpp
+++ Flipkart/Partition-To-K-Equal-Sum-Subsets.cpp
@@ -3,9 +3,15 @@ Explanation:
 If we need to divide the nums into k equal sum subsets then total sum should be divisible by K and number
 of elements in nums should be greater than K. We will use bit manipulation power set technique along with
 dynamic programming to cache the results.
-We will create mask of the power set to generate all combinations of sums and we will store the remainder
-by required Sum. If the remainder of the mask where all the elements are used is 0 then it is possible to 
-split into K equal sum subsets.
+We will create mask of the power set to generate all combinations of those states that can make the split 
+possible
+  Let's say final state is dp[111]
+    Now for transition to this state 3 states will be used: dp[011], dp[101] and dp[110]. 
+    We will try to add the elements which have still not been picked in these selection states and check 
+    which of the existing subset selection can help reach the final state where all the elements have 
+    been put in some partition.
+and we will store the remainder by required Sum. If the remainder of the mask where all the elements are 
+used is 0 then it is possible to split into K equal sum subsets.
 */
 
 class Solution {
